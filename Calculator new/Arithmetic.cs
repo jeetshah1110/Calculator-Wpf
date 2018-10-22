039ffee59a215f55273b414d224534f290d936f7
jeetshah1110 <36303896+jeetshah1110@users.noreply.github.com>
2018-10-22 15:55:22 +0530

    second commit

---
+++ Calculator new/Arithmetic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Arithmetic
+    {
+        public int no1, no2, no3;
+
+        public Arithmetic()
+        {
+            no1 = 0;
+            no2 = 0;
+            no3 = 0;
+        }
+        public Arithmetic(int val1, int val2)
+        {
+            no1 = val1;
+            no2 = val2;
+        }
+        public Arithmetic(int val1)
+        {
+            no3 = val1;
+        }
+        public int Add()
+        {
+            return no1 + no2;
+
+        }
+        public int Sub()
+        {
+            return no1 - no2;
+
+        }
+        public int mul()
+        {
+            return no1 * no2;
+
+        }
+        public int div()
+        {
+            return no1 / no2;
+        }
+        public string ConvertBin()
+        {
+            String Bin = Convert.ToString(no3, 2);
+            return Bin;
+        }
+        public string ConvertOct()
+        {
+            String Bin = Convert.ToString(no3, 8);
+            return Bin;
+        }
+        public string ConvertHex()
+        {
+            String Bin = Convert.ToString(no3, 16);
+            return Bin;
+        }
+    }
+}
--- Calculator new/MainWindow.xaml.cs
+++ Calculator new/MainWindow.xaml.cs
@@ -20,66 +20,16 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        int ip1, ip2;
+        char operation;
+        Arithmetic aobj;
         public MainWindow()
         {
+
+            aobj = new Arithmetic();
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            int ip1 = Convert.ToInt32(textbox1.Text);
-            int ip2 = Convert.ToInt32(textbox2.Text);
-
-            Arithmetic obj = new Arithmetic(ip1, ip2);
-            textbox3.Text = obj.Add().ToString();
-        }
-
-        private void textbox1_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void textbox2_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, RoutedEventArgs e)
-        {
-            int ip1 = Convert.ToInt32(textbox1.Text);
-            int ip2 = Convert.ToInt32(textbox2.Text);
-
-            Arithmetic obj = new Arithmetic(ip1, ip2);
-            textbox3.Text = obj.Sub().ToString();
-        }
-
-        private void button3_Click(object sender, RoutedEventArgs e)
-        {
-            int ip1 = Convert.ToInt32(textbox1.Text);
-            int ip2 = Convert.ToInt32(textbox2.Text);
-
-            Arithmetic obj = new Arithmetic(ip1, ip2);
-            textbox3.Text = obj.mul().ToString();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            int ip1 = Convert.ToInt32(textbox1.Text);
-            int ip2 = Convert.ToInt32(textbox2.Text);
-
-            Arithmetic obj = new Arithmetic(ip1, ip2);
-            textbox3.Text = obj.div().ToString();
-        }
-
-        private void button5_Click(object sender, RoutedEventArgs e)
-        {
-            int ip1 = Convert.ToInt32(textbox4.Text);
-
-            Arithmetic obj = new Arithmetic(ip1);
-            textbox5.Text = obj.ConvertBin();
-            textbox6.Text = obj.ConvertOct();
-            textbox7.Text = obj.ConvertHex();
-        }
+        
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
@@ -87,63 +37,89 @@ namespace Calculator
             textbox2.Text = "";
             textbox3.Text = "";
             textbox4.Text = "";
-            textbox5.Text = "";
-            textbox6.Text = "";
-            textbox7.Text = "";
+       }
+       
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textbox1.Text.Length != 0)
+            {
+                int no = Convert.ToInt32(textbox1.Text);
+                textbox2.Text = Convert.ToString(no, 2);
+                textbox3.Text = Convert.ToString(no, 8);
+                textbox4.Text = Convert.ToString(no, 16);
+            }
+            else
+            {
+                textbox2.Text = "";
+                textbox3.Text = "";
+                textbox4.Text = "";
+
+            }
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button obj = sender as Button;
+            string strip = obj.Content.ToString();
+          
+            if (strip[0] >= '0' && strip[0] <= '9')
+            {
+                textbox1.Text = textbox1.Text + strip;
+               
+            }
+            else if(strip[0]=='-' || strip[0] == '+' || strip[0] == '*' || strip[0] == '/')
+            {
+                operation = strip[0];
+                ip1 = Convert.ToInt32(textbox1.Text);
+                textbox1.Text = "";
+            }
+            else if(strip[0] == '=')
+            {
+                ip2 = Convert.ToInt32(textbox1.Text);
+                textbox1.Text = "";
+               
+
+                if (operation== '+')
+                {
+                    aobj.no1 = ip1;
+                    aobj.no2 = ip2;
+                    textbox1.Text = aobj.Add().ToString();
+
+                }
+                else if (operation == '-')
+                {
+                    aobj.no1 = ip1;
+                    aobj.no2 = ip2;
+                    textbox1.Text = aobj.Sub().ToString();
+
+                }
+                else if (operation == '*')
+                {
+                    aobj.no1 = ip1;
+                    aobj.no2 = ip2;
+                    textbox1.Text = aobj.mul().ToString();
+
+                }
+                else if (operation == '/')
+                {
+                    aobj.no1 = ip1;
+                    aobj.no2 = ip2;
+                    textbox1.Text = aobj.div().ToString();
+
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(textbox1.Text.Length!=0)
+            {
+                
+                textbox1.Text = textbox1.Text.Remove(textbox1.Text.Length - 1);
+            }
+        }
+
+       
     }
-    public class Arithmetic
-    {
-        public int no1, no2, no3;
-
-        public Arithmetic()
-        {
-            no1 = 0;
-            no2 = 0;
-            no3 = 0;
-        }
-        public Arithmetic(int val1, int val2)
-        {
-            no1 = val1;
-            no2 = val2;
-        }
-        public Arithmetic(int val1)
-        {
-            no3 = val1;
-        }
-        public int Add()
-        {
-            return no1 + no2;
-
-        }
-        public int Sub()
-        {
-            return no1 - no2;
-
-        }
-        public int mul()
-        {
-            return no1 * no2;
-
-        }
-        public int div()
-        {
-            return no1 / no2;
-        }
-        public string ConvertBin()
-        {
-            String Bin = Convert.ToString(no3, 2);
-            return Bin;
-        }
-        public string ConvertOct()
-        {
-            String Bin = Convert.ToString(no3, 8);
-            return Bin;
-        }
-        public string ConvertHex()
-        {
-            String Bin = Convert.ToString(no3, 16);
-            return Bin;
-        }
-    }
+ 
 }
