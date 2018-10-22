23189ef71a5077eab16c87d80cd17813b5f95e46
jeetshah1110 <36303896+jeetshah1110@users.noreply.github.com>
2018-10-22 16:02:55 +0530

    22 oct

--- Calculator new/Arithmetic.cs
+++ Calculator new/Arithmetic.cs
@@ -8,23 +8,19 @@ namespace Calculator
 {
     public class Arithmetic
     {
-        public int no1, no2, no3;
+        public int no1, no2;
 
         public Arithmetic()
         {
             no1 = 0;
             no2 = 0;
-            no3 = 0;
         }
         public Arithmetic(int val1, int val2)
         {
             no1 = val1;
             no2 = val2;
         }
-        public Arithmetic(int val1)
-        {
-            no3 = val1;
-        }
+     
         public int Add()
         {
             return no1 + no2;
@@ -44,20 +40,6 @@ namespace Calculator
         {
             return no1 / no2;
         }
-        public string ConvertBin()
-        {
-            String Bin = Convert.ToString(no3, 2);
-            return Bin;
-        }
-        public string ConvertOct()
-        {
-            String Bin = Convert.ToString(no3, 8);
-            return Bin;
-        }
-        public string ConvertHex()
-        {
-            String Bin = Convert.ToString(no3, 16);
-            return Bin;
-        }
+      
     }
 }
